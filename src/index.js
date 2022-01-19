1870ec8581e1b88e2a2962b632784a6f0e14996a
pavel shevrin <77666606+Shevrin@users.noreply.github.com>
2022-01-19 15:24:32 +0300

    add accordion

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import { scrollBtn } from './modules/scrollBtn'
 import { scrollPage } from './modules/scrollPage'
 import validation from './modules/validation'
 import { services } from './modules/services'
+import { accordion } from './modules/accordion'
 
 modal()
 scrollBtn()
 scrollPage()
 validation()
 services()
+accordion()
---
+++ src/modules/accordion.js
@@ -0,0 +1,20 @@
+export const accordion = () => {
+
+	const accordionBlock = document.querySelector('.accordeon')
+	const accordionElements = document.querySelectorAll('.accordeon > .element')
+	accordionBlock.addEventListener('click', (e) => {
+		accordionElements.forEach(element => {
+			if (element == e.target.closest('.element') && !element.classList.contains('active')) {
+				element.classList.toggle('active')
+				element.children[1].style.display = 'block'
+
+			} else {
+				// element.classList.toggle('active')
+				element.classList.remove('active')
+				element.children[1].style.display = 'none'
+
+			}
+		});
+		// }
+	})
+}
