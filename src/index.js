dc3d6dce3f65c8246026d492f905684902b851dd
pavel shevrin <77666606+Shevrin@users.noreply.github.com>
2022-01-19 22:28:19 +0300

    add main slider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import { modal } from './modules/modal'
 import { scrollBtn } from './modules/scrollBtn'
 import { scrollPage } from './modules/scrollPage'
-import validation from './modules/validation'
 import { services } from './modules/services'
 import { accordion } from './modules/accordion'
+import { sliderMain } from './modules/sliderMain'
+import validation from './modules/validation'
 
 modal()
 scrollBtn()
 scrollPage()
-validation()
 services()
 accordion()
+sliderMain()
+validation()
